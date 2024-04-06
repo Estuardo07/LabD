8199c2740e54060f7521385f5fe012dccbe546cc
Estuardo07 <xavihernand7@hotmail.com>
2024-04-05 23:31:48 -0600

    main program and .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#
+from yalex_reader import Yalex_reader
+from afdd import regex_to_afd
+from jinja2 import Template
+import pickle
+
+
+def main():
+    archivo_yalex = 'slr-2.yal'
+    rule_token, token_dic = Yalex_reader(archivo_yalex)
+    with open('template.j2', 'r') as f:
+        template = f.read()
+
+    template = Template(template)
+    rendered = template.render(tokens=token_dic)
+    with open('scanner.py', 'w') as f:
+        f.write(rendered)
+    afd = regex_to_afd(rule_token, token_dic)
+
+    print("afd generado")
+    with open('afd.pickle', 'wb') as f:
+        pickle.dump(afd, f)
+
+
+if __name__ == '__main__':
+    main()
